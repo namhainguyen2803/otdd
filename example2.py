18db00b70b41cd1b28d5e2695e13b42c9bf51c28
namhainguyen2803 <hai.nn214894@sis.hust.edu.vn>
2024-07-21 08:07:25 +0000

    commit

--- example2.py
+++ example2.py
@@ -2,17 +2,21 @@ from otdd.pytorch.datasets import load_torchvision_data
 from otdd.pytorch.method import NewDatasetDistance
 
 # Load data
-loaders_src  = load_torchvision_data('MNIST', valid_size=0, resize = 28, maxsize=None)[0]
-loaders_tgt  = load_torchvision_data('FashionMNIST',  valid_size=0, resize = 28, maxsize=None)[0]
+loaders_src  = load_torchvision_data('KMNIST', valid_size=0, resize = 28, maxsize=None)[0]
+loaders_tgt  = load_torchvision_data('USPS',  valid_size=0, resize = 28, maxsize=None, datadir="data/USPS")[0]
+
+
+
+
 
 # Instantiate distance
-dist = NewDatasetDistance(loaders_src['train'], 
-                          loaders_tgt['train'],
-                          p=2,
-                          device='cpu')
+# dist = NewDatasetDistance(loaders_src['train'], 
+#                           loaders_tgt['train'],
+#                           p=2,
+#                           device='cpu')
 
-d = dist.distance(maxsamples=None, num_projection=5000)
-print(f'OTDD(MNIST,FashionMNIST)={d:8.2f}')
+# d = dist.distance(maxsamples=None, num_projection=5000)
+# print(f'OTDD(MNIST,FashionMNIST)={d:8.2f}')
 
 # print(len(loaders_src['train']), len(loaders_tgt['train']))
 
