d5df1622b50f85c62aff5a9ce60e733b4804ad2f
namhainguyen2803 <hai.nn214894@sis.hust.edu.vn>
2024-09-14 04:22:44 +0000

    h

--- scripts/text_cls_spp/pretrain/run1.sh
+++ scripts/text_cls_spp/pretrain/run1.sh
@@ -9,10 +9,14 @@
 #SBATCH --partition=research
 #SBATCH --mail-type=all
 #SBATCH --mail-user=v.HaiNN14@vinai.io
+
 module purge
 module load python/miniconda3/miniconda3
-eval “$(conda shell.bash hook)”
-conda activate /lustre/scratch/client/vinai/users/hainn14/otdd
+
+# Corrected line
+eval "$(conda shell.bash hook)"
+
+conda activate /lustre/scratch/client/vinai/users/hainn14/envs/otdd2
 cd /lustre/scratch/client/vinai/users/hainn14/otdd
 
 dataset="AG_NEWS"
