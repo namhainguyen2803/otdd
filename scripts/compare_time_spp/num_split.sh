e69f82d9d787c259804e7badb918a3f0488aa392
namhainguyen2803 <hai.nn214894@sis.hust.edu.vn>
2024-10-18 13:26:47 +0000

    h

---
+++ scripts/compare_time_spp/num_split.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -e
+#SBATCH --job-name=num_split
+#SBATCH --output=/lustre/scratch/client/vinai/users/hainn14/otdd/num_split.out
+#SBATCH --error=/lustre/scratch/client/vinai/users/hainn14/otdd/num_split.err
+#SBATCH --nodes=1
+#SBATCH --gpus-per-node=1
+#SBATCH --mem-per-gpu=125G
+#SBATCH --cpus-per-gpu=32
+#SBATCH --partition=research
+#SBATCH --mail-type=all
+#SBATCH --mail-user=v.HaiNN14@vinai.io
+
+python split_cifar3.py --num_splits 2 --split_size 5000 --num_projections 10000 --num_classes 100
+python split_cifar3.py --num_splits 3 --split_size 5000 --num_projections 10000 --num_classes 100
+python split_cifar3.py --num_splits 4 --split_size 5000 --num_projections 10000 --num_classes 100
+python split_cifar3.py --num_splits 5 --split_size 5000 --num_projections 10000 --num_classes 100
+python split_cifar3.py --num_splits 6 --split_size 5000 --num_projections 10000 --num_classes 100
+python split_cifar3.py --num_splits 7 --split_size 5000 --num_projections 10000 --num_classes 100
+python split_cifar3.py --num_splits 8 --split_size 5000 --num_projections 10000 --num_classes 100
+python split_cifar3.py --num_splits 9 --split_size 5000 --num_projections 10000 --num_classes 100
+python split_cifar3.py --num_splits 10 --split_size 5000 --num_projections 10000 --num_classes 100
---
+++ scripts/compare_time_spp/split_size.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -e
+#SBATCH --job-name=split_size
+#SBATCH --output=/lustre/scratch/client/vinai/users/hainn14/otdd/split_size.out
+#SBATCH --error=/lustre/scratch/client/vinai/users/hainn14/otdd/split_size.err
+#SBATCH --nodes=1
+#SBATCH --gpus-per-node=1
+#SBATCH --mem-per-gpu=125G
+#SBATCH --cpus-per-gpu=32
+#SBATCH --partition=research
+#SBATCH --mail-type=all
+#SBATCH --mail-user=v.HaiNN14@vinai.io
+
+python split_cifar3.py --num_splits 2 --split_size 200 --num_projections 10000 --num_classes 100
+python split_cifar3.py --num_splits 2 --split_size 500 --num_projections 10000 --num_classes 100
+python split_cifar3.py --num_splits 2 --split_size 1000 --num_projections 10000 --num_classes 100
+python split_cifar3.py --num_splits 2 --split_size 2000 --num_projections 10000 --num_classes 100
+python split_cifar3.py --num_splits 2 --split_size 5000 --num_projections 10000 --num_classes 100
+python split_cifar3.py --num_splits 2 --split_size 8000 --num_projections 10000 --num_classes 100
+python split_cifar3.py --num_splits 2 --split_size 10000 --num_projections 10000 --num_classes 100
