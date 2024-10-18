166e5298d3a173b7dabc598bcc2fb733ba8a3640
namhainguyen2803 <hai.nn214894@sis.hust.edu.vn>
2024-10-18 13:20:00 +0000

    h

---
+++ scripts/compare_time/num_split.sh
@@ -0,0 +1,9 @@
+python split_cifar3.py --num_splits 2 --split_size 5000 --num_projections 10000 --num_classes 100
+python split_cifar3.py --num_splits 3 --split_size 5000 --num_projections 10000 --num_classes 100
+python split_cifar3.py --num_splits 4 --split_size 5000 --num_projections 10000 --num_classes 100
+python split_cifar3.py --num_splits 5 --split_size 5000 --num_projections 10000 --num_classes 100
+python split_cifar3.py --num_splits 6 --split_size 5000 --num_projections 10000 --num_classes 100
+python split_cifar3.py --num_splits 7 --split_size 5000 --num_projections 10000 --num_classes 100
+python split_cifar3.py --num_splits 8 --split_size 5000 --num_projections 10000 --num_classes 100
+python split_cifar3.py --num_splits 9 --split_size 5000 --num_projections 10000 --num_classes 100
+python split_cifar3.py --num_splits 10 --split_size 5000 --num_projections 10000 --num_classes 100
