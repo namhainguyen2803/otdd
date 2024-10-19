513bf56a7cd90030db72dda794115f08164b2bd7
namhainguyen2803 <hai.nn214894@sis.hust.edu.vn>
2024-10-19 09:08:11 +0000

    h

--- scripts/compare_time_spp/split_size_3.sh
+++ scripts/compare_time_spp/split_size_3.sh
@@ -1,4 +1,4 @@
-parent_dir="saved_cpu"
+parent_dir="saved_cpu_2"
 exp_type="split_size"
 python split_cifar3.py --parent_dir "$parent_dir" --exp_type "$exp_type" --num_splits 2 --split_size 1000 --num_projections 10000 --num_classes 100
 python split_cifar3.py --parent_dir "$parent_dir" --exp_type "$exp_type" --num_splits 2 --split_size 2000 --num_projections 10000 --num_classes 100
