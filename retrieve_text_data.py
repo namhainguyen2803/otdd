ac6df3659247dbcf8ec14b77f63962c9c83103c2
namhainguyen2803 <hai.nn214894@sis.hust.edu.vn>
2024-11-27 18:31:38 +0000

    h

--- retrieve_text_data.py
+++ retrieve_text_data.py
@@ -41,12 +41,12 @@ def save_data(data_set, saved_tensor_path):
     list_images = list()
     list_labels = list()
     for img, label in data_set:
-        list_images.append(img)
+        list_images.append(img.squeeze(1))
         list_labels.append(label)
-    tensor_images = torch.stack(list_images)
+    tensor_images = torch.cat(list_images, dim=0)
     tensor_labels = torch.tensor(list_labels)
     torch.save((tensor_images, tensor_labels), saved_tensor_path)
-    print(f"Number of data: {len(list_images)}, Save data into {saved_tensor_path}")
+    print(f"Number of data: {len(list_images)}, data shape: {tensor_images.shape, tensor_labels.shape}. Save data into {saved_tensor_path}")
 
 
 DATASET_NAMES = ["AG_NEWS", "DBpedia", "YelpReviewPolarity", "YelpReviewFull", "YahooAnswers", "AmazonReviewPolarity", "AmazonReviewFull"]
@@ -56,5 +56,5 @@ os.makedirs(parent_dir, exist_ok=True)
 
 
 for dataset_name in DATASET_NAMES:
-    train_dataset = load_textclassification_data(dataset_name, maxsize=None, load_tensor=True)[0]
+    train_dataset = load_textclassification_data(dataset_name, maxsize=10000, load_tensor=True)[0]
     save_data(data_set=train_dataset, saved_tensor_path=f"{parent_dir}/{dataset_name}_text.pt")
