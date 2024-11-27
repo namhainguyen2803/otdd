ae2c3b5583f63544b1ddc7c01c6bf35af7e5804c
namhainguyen2803 <hai.nn214894@sis.hust.edu.vn>
2024-11-27 13:16:12 +0000

    h

--- run_mnist_parts.sh
+++ run_mnist_parts.sh
@@ -1,5 +1,5 @@
-python3 split_mnist_parts.py --parent_dir saved_runtime_mnist_vietdt11_parts --method sotdd
-python3 split_mnist_parts.py --parent_dir saved_runtime_mnist_vietdt11_parts --method otdd_exact
-python3 split_mnist_parts.py --parent_dir saved_runtime_mnist_vietdt11_parts --method otdd_ga
-python3 split_mnist_parts.py --parent_dir saved_runtime_mnist_vietdt11_parts --method wte
-python3 split_mnist_parts.py --parent_dir saved_runtime_mnist_vietdt11_parts --method hswfs
+python3 split_mnist_parts.py --parent_dir saved_runtime_mnist_parts --method sotdd
+python3 split_mnist_parts.py --parent_dir saved_runtime_mnist_parts --method otdd_exact
+python3 split_mnist_parts.py --parent_dir saved_runtime_mnist_parts --method otdd_ga
+python3 split_mnist_parts.py --parent_dir saved_runtime_mnist_parts --method wte
+python3 split_mnist_parts.py --parent_dir saved_runtime_mnist_parts --method hswfs
